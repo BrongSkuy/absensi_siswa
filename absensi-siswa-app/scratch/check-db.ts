feba7b93b06ba7403c03bd6c8b43142f0f9cc183
BrongSkuy <faizrabbani179@gmail.com>
2026-05-25 09:49:24 +0700

    chore: update scratch/check-db.ts to use new database schema

--- absensi-siswa-app/scratch/check-db.ts
+++ absensi-siswa-app/scratch/check-db.ts
@@ -1,16 +1,19 @@
+import "dotenv/config";
 import { db } from "../src/db/index";
-import { teachers, teacherClasses, teacherSubjects, classes } from "../src/db/schema";
+import { teachers, subjects, classes } from "../src/db/schema";
 
 
 async function checkAssignments() {
   const allTeachers = await db.select().from(teachers).all();
   console.log("Teachers:", allTeachers.map(t => ({ id: t.id, name: t.namaLengkap })));
 
-  const allTeacherClasses = await db.select().from(teacherClasses).all();
-  console.log("Teacher Classes Assignments:", allTeacherClasses);
-
-  const allTeacherSubjects = await db.select().from(teacherSubjects).all();
-  console.log("Teacher Subjects Assignments:", allTeacherSubjects);
+  const allSubjects = await db.select().from(subjects).all();
+  console.log("Subjects (includes Teacher ID and Classes):", allSubjects.map(s => ({
+    id: s.id,
+    name: s.namaMapel,
+    teacherId: s.teacherId,
+    classes: s.kelasDiampu
+  })));
   
   const allClasses = await db.select().from(classes).all();
   console.log("Classes:", allClasses.map(c => ({ name: c.namaKelas, wali: c.waliKelas })));
